ab50ac0d27b79594a288860d1e0cef1f712f1eed
mangila <john.doe@mail.com>
2025-01-19 12:43:50 +0100

    pokemon it test

---
+++ pokedex-integration-test/Controller/PokemonControllerTest.cs
@@ -0,0 +1,15 @@
+﻿using pokedex_api.Controller;
+
+namespace pokedex_integration_test.Controller;
+
+[TestFixture]
+[TestOf(typeof(PokemonController))]
+public class PokemonControllerTest
+{
+
+    [Test]
+    public void METHOD()
+    {
+        
+    }
+}
