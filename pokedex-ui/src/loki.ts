4a83d77e03dda2cb733df4587769a9ecb987b9f1
mangila <john.doe@mail.com>
2025-01-21 15:45:21 +0100

    loki in ui

--- pokedex-ui/src/loki.ts
+++ pokedex-ui/src/loki.ts
@@ -1,12 +1,13 @@
 ﻿import winston from "winston";
 import LokiTransport from "winston-loki";
+import * as process from "node:process";
 
 // Loki transport configuration
 const loki = winston.createLogger({
     transports: [
         new LokiTransport({
             host: "http://localhost:3100",
-            labels: {app: "pokedex-ui"},
+            labels: {app: "pokedex-ui", env: process.env.NODE_ENV},
             json: true,
             format: winston.format.json(),
             onConnectionError: (err) => {
