1ef27fea78a7e0613bd40b1d572f83819ceb180e
mangila <olsson.erik1993@gmail.com>
2025-02-07 13:15:57 +0100

    add fav pokemon

--- pokedex-ui/src/components/FavoriteButton.tsx
+++ pokedex-ui/src/components/FavoriteButton.tsx
@@ -2,15 +2,15 @@
 import IconButton from '@mui/material/IconButton';
 import Favorite from '@mui/icons-material/Favorite';
 import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
-import {AppendFavorites, RemoveFavorites} from '@shared/utils';
+import {AppendFavorites, HasFavorites, RemoveFavorites} from '@shared/utils';
 
 interface Props {
     id: number;
 }
 
 export default function FavoriteButton({id}: Props) {
-    const [favorite, setFavorite] = useState<boolean>(false);
-    
+    const [favorite, setFavorite] = useState<boolean>(HasFavorites(id));
+
     const handleClick = () => {
         if (favorite) {
             RemoveFavorites(id);
