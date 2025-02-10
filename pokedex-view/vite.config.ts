59c6cde0c2e65cf8e051901903722ebc5fc08648
mangila <olsson.erik1993@gmail.com>
2025-02-10 17:29:27 +0100

    build web app

--- pokedex-view/vite.config.ts
+++ pokedex-view/vite.config.ts
@@ -2,14 +2,13 @@ import {defineConfig} from 'vite'
 import {readFileSync} from 'fs';
 import {resolve} from 'path';
 import react from '@vitejs/plugin-react-swc'
-import {execSync} from 'child_process';
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
     define: {
         __APP_VERSION__: JSON.stringify(getAppVersion()),
-        __BUILD_ID__: JSON.stringify(getCommitHash())
+        __BUILD_ID__: Date.now().toString(),
     },
     resolve: {
         alias: {
@@ -26,10 +25,5 @@ function getAppVersion(): string {
     const packageJson = readFileSync(resolve(".", 'package.json'), 'utf-8');
     const {version} = JSON.parse(packageJson);
     return version;
-}
-
-// Function to get the latest commit hash
-function getCommitHash() {
-    return execSync('git rev-parse HEAD').toString().trim();
 }
 
